2dd2c163b9b77e97f4a7c2ff690bab09d01e1a35
Walter Hausleitner <whausleitner@lr.htlweiz.at>
2019-12-10 20:24:04 +0100

    Meine Commit-Nachricht

---
+++ myCube/myCube/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Geben Sie die Kantenlänge ein: ");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Das Volumen beträgt" + a + "Volums-Einheiten!");
+        }
+    }
+}
